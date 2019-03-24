e2dd34d4437ff2a55b9862a9d6883f41cfb745f1
Ali M'rabeth <ali.mrabeth@student-cs.fr>
2019-03-24 15:43:03 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -20,25 +20,24 @@ def process(lines):
         - sentence
         '''
     data = pd.DataFrame(lines, columns = ['polarity', 'aspect', 'term', 'offsets', 'sentence'])
-        
-    polarity = pd.get_dummies(data['polarity'], drop_first=True)
-    aspect = pd.get_dummies(data['aspect'], drop_first=True)
-    data = pd.concat([data, polarity, aspect], axis=1)
-        
-    #tokens, data = tokenize(data)
     
-    #TODO word2vec, then add ONLY necessary features to 'data'
+    target = pd.get_dummies(data['polarity'])
     
     # the function below create a new column 'sentiment_terms that :
     # - extract from sentences adj, verb and adverb
     # - without punctuation nor stop words
     # - and lemmatized
-    sentiment_terms(data)
+    create_sentiment_terms(data, column_name='sentence')
+    
+    #we extract from the column 'sentiment_terms', the terms in a window of lenght = window_size 
+    window_size=4
+    data['words_in_window'] = [ extract_window_words(row['sentiment_terms'],row['term'],window_size ) for i,row in data.iterrows()  ]
     
     #the function below creates an average vector of word2vec from words in the column indicated
-    vector_context(data, column_name='sentiment_terms')
+    #data['avg_word2vec'] = sent2vec(data, column_name='words_in_window')
     
-    return data
+    features = sent2vec(data, column_name='words_in_window')
+    return features, target
     
 def tokenize(data):
     '''Count the occurrences of each POS tag in a sentence'''
@@ -87,19 +86,13 @@ def bow(sentence):
     tokens = [t.lower() for t in tokens]
         
     return tokens, punct
-    
-def ngram(sentences, n = 2):
-    '''Studies the most common combination of words'''
-    pass
 
 
-
-
-def sentiment_terms (data) :
+def create_sentiment_terms (data, column_name) :
     ##function that extract from sentences adj, verb and adverb, without punctuation nor stop words, and lemmatized
-    nlp = spacy.load('en')
+    nlp = spacy.load('en_core_web_sm')
     sentiment_terms = []
-    for sentence in nlp.pipe(data['sentence']):
+    for sentence in nlp.pipe(data[column_name]):
         if sentence.is_parsed:
             #sentiment_terms.append(' '.join([token.lemma_ for token in sentence if ( not token.is_stop and not token.is_punct and token.pos_ in ["ADJ", "VERB","ADV",'NOUN'] )]) )
             tag_list = ['NN','NNS','NNP','NNPS','RB','RBR','RBS','JJ','JJR','JJS','VB','VBD','VBG','VBN','VBP','VBZ']
@@ -108,10 +101,10 @@ def sentiment_terms (data) :
             sentiment_terms.append('')  
     data['sentiment_terms'] = sentiment_terms
 
-def vector_context(data, column_name):
+def sent2vec(data, column_name):
     ''' Take in a dataframe, and return the list of vector average of the sentences
     using 'en_core_web_sm' from spacy '''
-    
+    np.random.seed(1)
     #Loading 'en_core_web_sm' from spacy
     nlp = spacy.load('en_core_web_sm')
     
@@ -119,18 +112,69 @@ def vector_context(data, column_name):
     avg_word2vec = []
     
     #Creting the vector average for each sentence
+    embedding_size = len( nlp('be').vector )
+    vect_aleatoire = np.random.rand(embedding_size)*10
     for index, row in data.iterrows():
-        vector = np.zeros(384) #initializing the sum of vector
+        vector = np.zeros(embedding_size) #initializing the sum of vector
         length = 0 # number of words in the sentence
-        
         # Getting the vector for each word in the sentence and adding them together
-        for word in nlp(row[column_name]):
-                vector += word.vector
+        for word in row[column_name]:
+                vector += nlp(word).vector
                 length +=1
-                
-        vector_average = vector / length # Dividing the sum of vectors to obtain the average
+        if length :       
+            vector_average = vector / length # Dividing the sum of vectors to obtain the average
+        else :
+            vector_average = vect_aleatoire
         avg_word2vec.append(vector_average) 
-    data['avg_word2vec'] = avg_word2vec
+    return np.array(avg_word2vec)
     
+def extract_window_words(sentence, aspect_terms, window_Size ) :
+    """ This method takes as input a sentence, and returns list of tuples :
+        - the first element is the center word
+        - the second is a list of context words
+    """
+    sentences_list = sentence.split(' ')
+    L = len(sentences_list)
+    aspect_context = []
+    for index, word in enumerate(sentences_list):
+        inf = index - window_Size
+        sup = index + window_Size + 1
+        
+        if word in aspect_terms :
+            context = [sentences_list[i] for i in range(inf, sup) if 0 <= i < L and i != index and sentences_list[i] not in aspect_context]
+            for c in context :
+                aspect_context.append(c)
+    return aspect_context
 
+
+
+path= "/Users/alimrabeth/Desktop/Master Data Sciences & Business Analytics/Data Sciences Elective courses/NLP/Projet 2/exercise2/data/traindata.csv"
+lines = retrieveData(path)
+
+x_train,y_train = process(lines)
+
+dev_path = "/Users/alimrabeth/Desktop/Master Data Sciences & Business Analytics/Data Sciences Elective courses/NLP/Projet 2/exercise2/data/devdata.csv"
+x_dev, y_dev = process(retrieveData(dev_path))
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Conv1D
+
+shape = x_train.shape[1]
+nn_model = Sequential()
+nn_model.add(Dense(128, input_shape=(shape,), activation='relu'))
+nn_model.add(Dense(64, activation='relu'))
+nn_model.add(Dense(3, activation='softmax'))
+nn_model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+nn_model.fit(x= x_train, y=y_train ,epochs=50, validation_data =(x_dev, y_dev))
+
+x_train = np.expand_dims(x_train, axis=2) # reshape (569, 30) to (569, 30, 1)
+
+nn_model = Sequential()
+nn_model.add(Conv1D(64, 5, input_shape=(384,1)))
+nn_model.add(Dense(64, activation='relu'))
+nn_model.add(Dense(3, activation='softmax'))
+nn_model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+nn_model.fit(x= x_train, y=y_train ,epochs=50)#, validation_data =(x_dev, y_dev))
 
